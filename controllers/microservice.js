2e9e6664efe6a15770ce5060a7c1e2241d707eea
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-11-20 12:05:51 -0500

    Set up controllers with the requested endpoints

---
+++ controllers/microservice.js
@@ -0,0 +1,13 @@
+const Microservice = require("../models/Microservice");
+
+// Return a specific user (and all associated information)
+exports.getUser = (req, res) => {};
+
+// Return a list of users created between a date range
+exports.getAllUsersByDateRange = (req, res) => {};
+
+// Return a list of users based on a specific profession
+exports.getAllUsersByProfession = (req, res) => {};
+
+// Return a list of users based on a specific country
+exports.getAllUsersByCountry = (req, res) => {};
