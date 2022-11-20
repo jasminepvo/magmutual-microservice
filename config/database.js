0c107feafac9462041c36bbb91347a67fc1dc1ce
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-11-20 11:28:07 -0500

    Set up MVC architecture and connect to db

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.DB_URL);
+		console.log("Connected to DB");
+	} catch (err) {
+		console.log(err);
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const connectDB = require("./config/database");
+require("dotenv").config({ path: "./config/.env" });
+
+// Enable cors
+var corsOptions = {
+	origin: "http://localhost:3002",
+};
+app.use(cors(corsOptions));
+
+// Connect to the database
+connectDB();
+
+const hello = "Hello MagMutual";
+
+// Set Routes
+app.get("/", (req, res) => {
+	res.json(hello);
+});
+
+// Set PORT, listen for requests
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
